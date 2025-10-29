0df2da49220db4c7fc1e591b9d06516861430893
Chase Miner <chasetminer@gmail.com>
2025-10-29 09:36:05 -0600

    changed vite.config.js

--- react-site/vite.config.js
+++ react-site/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-site/',
+  // base: '/react-site/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
